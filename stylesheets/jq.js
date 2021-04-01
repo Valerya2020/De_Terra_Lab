a448b7dc3d5759eafb41dcbe07e4a272f0a2b0d7
Valerya2020 <svr2014@mail.ru>
2021-04-02 02:23:54 +0300

    main

--- stylesheets/jq.js
+++ stylesheets/jq.js
@@ -53,59 +53,78 @@ $(document).ready(function(){
         $(".glass_dialogue_red").addClass("hide");
         $(".red_reagent").addClass("red_reagent_90");
         $(".red_reagent").removeClass("red_reagent");
+        $(".plesen1").removeClass("hide");
       }
       if (count_1 == 3) {
       $(".glass_dialogue_red").addClass("hide");
       $(".red_reagent_90").addClass("red_reagent_80");
       $(".red_reagent_90").removeClass("red_reagent_90");
+      $(".plesen2").removeClass("hide");
       }
       if (count_1 == 5) {
       $(".glass_dialogue_red").addClass("hide");
       $(".red_reagent_80").addClass("red_reagent_70");
       $(".red_reagent_80").removeClass("red_reagent_80");
+      $(".plesen1").addClass("hide");
+      $(".plesen11").removeClass("hide");
       }
       if (count_1 == 9) {
       $(".glass_dialogue_red").addClass("hide");
       $(".red_reagent_70").addClass("red_reagent_60");
       $(".red_reagent_70").removeClass("red_reagent_70");
+      $(".plesen2").addClass("hide");
+      $(".plesen12").removeClass("hide");
       }
       if (count_1 == 13) {
       $(".glass_dialogue_red").addClass("hide");
       $(".red_reagent_60").addClass("red_reagent_50");
       $(".red_reagent_60").removeClass("red_reagent_60");
+      $(".plesen13").removeClass("hide");
       }
       if (count_1 == 21) {
       $(".glass_dialogue_red").addClass("hide");
       $(".red_reagent_50").addClass("red_reagent_40");
       $(".red_reagent_50").removeClass("red_reagent_50");
+      $(".plesen14").removeClass("hide");
       }
       if (count_1 == 28) {
       $(".glass_dialogue_red").addClass("hide");
       $(".red_reagent_40").addClass("red_reagent_30");
       $(".red_reagent_40").removeClass("red_reagent_40");
+      $(".plesen15").removeClass("hide");
       }
       if (count_1 == 38) {
       $(".glass_dialogue_red").addClass("hide");
       $(".red_reagent_30").addClass("red_reagent_20");
       $(".red_reagent_30").removeClass("red_reagent_30");
+      $(".plesen13").addClass("hide");
+      $(".plesen14").addClass("hide");
+      $(".plesen27").removeClass("hide");
       }
       if (count_1 == 50) {
       $(".glass_dialogue_red").addClass("hide");
       $(".red_reagent_20").addClass("red_reagent_10");
       $(".red_reagent_20").removeClass("red_reagent_20");
+      $(".plesen28").removeClass("hide");
       }
       if (count_1 == 61) {
       $(".glass_dialogue_red").addClass("hide");
       $(".red_reagent_10").addClass("red_reagent_0");
       $(".red_reagent_10").removeClass("red_reagent_10");
+      $(".plesen15").addClass("hide");
+      $(".plesen28").addClass("hide");
+      $(".plesen29").removeClass("hide");
       }
       if (count_1 == 72) {
       $(".glass_dialogue_red").addClass("hide");
       $(".red_reagent_0").addClass("red_reagent_00");
       $(".red_reagent_0").removeClass("red_reagent_0");
+      $(".plesen30").removeClass("hide");
       }
       if (count_1 >= 83) {
       $(".glass_dialogue_red").addClass("hide");
+      $(".plesen27").addClass("hide");
+      $(".plesen31").removeClass("hide");
       }
         });
 
@@ -128,59 +147,73 @@ $(document).ready(function(){
           $(".glass_dialogue_green").addClass("hide");
           $(".green_reagent").addClass("green_reagent_90");
           $(".green_reagent").removeClass("green_reagent");
+          $(".plesen3").removeClass("hide");
         }
         if (count_2 == 3) {
         $(".glass_dialogue_green").addClass("hide");
         $(".green_reagent_90").addClass("green_reagent_80");
         $(".green_reagent_90").removeClass("green_reagent_90");
+        $(".plesen4").removeClass("hide");
         }
         if (count_2 == 5) {
         $(".glass_dialogue_green").addClass("hide");
         $(".green_reagent_80").addClass("green_reagent_70");
         $(".green_reagent_80").removeClass("green_reagent_80");
+        $(".plesen16").removeClass("hide");
         }
         if (count_2 == 9) {
         $(".glass_dialogue_green").addClass("hide");
         $(".green_reagent_70").addClass("green_reagent_60");
         $(".green_reagent_70").removeClass("green_reagent_70");
+        $(".plesen17").removeClass("hide");
         }
         if (count_2 == 13) {
         $(".glass_dialogue_green").addClass("hide");
         $(".green_reagent_60").addClass("green_reagent_50");
         $(".green_reagent_60").removeClass("green_reagent_60");
+        $(".plesen18").removeClass("hide");
         }
         if (count_2 == 21) {
         $(".glass_dialogue_green").addClass("hide");
         $(".green_reagent_50").addClass("green_reagent_40");
         $(".green_reagent_50").removeClass("green_reagent_50");
+        $(".plesen19").removeClass("hide");
         }
         if (count_2 == 28) {
         $(".glass_dialogue_green").addClass("hide");
         $(".green_reagent_40").addClass("green_reagent_30");
         $(".green_reagent_40").removeClass("green_reagent_40");
+        $(".plesen16").addClass("hide");
+        $(".plesen20").removeClass("hide");
         }
         if (count_2 == 38) {
         $(".glass_dialogue_green").addClass("hide");
         $(".green_reagent_30").addClass("green_reagent_20");
         $(".green_reagent_30").removeClass("green_reagent_30");
+        $(".plesen32").removeClass("hide");
         }
         if (count_2 == 50) {
         $(".glass_dialogue_green").addClass("hide");
         $(".green_reagent_20").addClass("green_reagent_10");
         $(".green_reagent_20").removeClass("green_reagent_20");
+        $(".plesen20").addClass("hide");
+        $(".plesen33").removeClass("hide");
         }
         if (count_2 == 61) {
         $(".glass_dialogue_green").addClass("hide");
         $(".green_reagent_10").addClass("green_reagent_0");
         $(".green_reagent_10").removeClass("green_reagent_10");
+        $(".plesen17").removeClass("hide");
         }
         if (count_2 == 72) {
         $(".glass_dialogue_green").addClass("hide");
         $(".green_reagent_0").addClass("green_reagent_00");
         $(".green_reagent_0").removeClass("green_reagent_0");
+        $(".plesen18").removeClass("hide");
         }
         if (count_2 >= 83) {
         $(".glass_dialogue_green").addClass("hide");
+        $(".plesen19").removeClass("hide");
         }
           });
 
@@ -203,59 +236,76 @@ $(document).ready(function(){
             $(".glass_dialogue_blue").addClass("hide");
             $(".blue_reagent").addClass("blue_reagent_90");
             $(".blue_reagent").removeClass("blue_reagent");
+            $(".plesen5").removeClass("hide");
           }
           if (count_3 == 3) {
           $(".glass_dialogue_blue").addClass("hide");
           $(".blue_reagent_90").addClass("blue_reagent_80");
           $(".blue_reagent_90").removeClass("blue_reagent_90");
+          $(".plesen6").removeClass("hide");
           }
           if (count_3 == 5) {
           $(".glass_dialogue_blue").addClass("hide");
           $(".blue_reagent_80").addClass("blue_reagent_70");
           $(".blue_reagent_80").removeClass("blue_reagent_80");
+          $(".plesen7").removeClass("hide");
           }
           if (count_3 == 9) {
           $(".glass_dialogue_blue").addClass("hide");
           $(".blue_reagent_70").addClass("blue_reagent_60");
           $(".blue_reagent_70").removeClass("blue_reagent_70");
+          $(".plesen8").removeClass("hide");
           }
           if (count_3 == 13) {
           $(".glass_dialogue_blue").addClass("hide");
           $(".blue_reagent_60").addClass("blue_reagent_50");
           $(".blue_reagent_60").removeClass("blue_reagent_60");
+          $(".plesen9").removeClass("hide");
           }
           if (count_3 == 21) {
           $(".glass_dialogue_blue").addClass("hide");
           $(".blue_reagent_50").addClass("blue_reagent_40");
           $(".blue_reagent_50").removeClass("blue_reagent_50");
+          $(".plesen5").addClass("hide");
+          $(".plesen10").removeClass("hide");
           }
           if (count_3 == 28) {
           $(".glass_dialogue_blue").addClass("hide");
           $(".blue_reagent_40").addClass("blue_reagent_30");
           $(".blue_reagent_40").removeClass("blue_reagent_40");
+          $(".plesen9").addClass("hide");
+          $(".plesen21").removeClass("hide");
           }
           if (count_3 == 38) {
           $(".glass_dialogue_blue").addClass("hide");
           $(".blue_reagent_30").addClass("blue_reagent_20");
           $(".blue_reagent_30").removeClass("blue_reagent_30");
+          $(".plesen22").removeClass("hide");
           }
           if (count_3 == 50) {
           $(".glass_dialogue_blue").addClass("hide");
           $(".blue_reagent_20").addClass("blue_reagent_10");
           $(".blue_reagent_20").removeClass("blue_reagent_20");
+          $(".plesen23").removeClass("hide");
           }
           if (count_3 == 61) {
           $(".glass_dialogue_blue").addClass("hide");
           $(".blue_reagent_10").addClass("blue_reagent_0");
           $(".blue_reagent_10").removeClass("blue_reagent_10");
+          $(".plesen22").addClass("hide");
+          $(".plesen24").removeClass("hide");
           }
           if (count_3 == 72) {
           $(".glass_dialogue_blue").addClass("hide");
           $(".blue_reagent_0").addClass("blue_reagent_00");
           $(".blue_reagent_0").removeClass("blue_reagent_0");
+          $(".plesen25").removeClass("hide");
           }
           if (count_3 >= 83) {
           $(".glass_dialogue_blue").addClass("hide");
+          $(".plesen23").addClass("hide");
+          $(".plesen24").addClass("hide");
+          $(".plesen26").removeClass("hide");
           }
             });
 
